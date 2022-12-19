ce630aea7e70af65c39fdb11eb57c3c632d56562
Jesse Strickland <112838688+Skinniez@users.noreply.github.com>
2022-12-19 14:29:50 -0500

    initial

---
+++ src/dishes/dishes.controller.js
@@ -0,0 +1,90 @@
+const path = require("path");
+// Use the existing dishes data
+const dishes = require(path.resolve("src/data/dishes-data"));
+// Use this function to assign ID's when necessary
+const nextId = require("../utils/nextId");
+// TODO: Implement the /dishes handlers needed to make the tests pass
+
+function dishIdExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish id not found: ${dishId}`,
+  });
+}
+
+function dishIsValid(req, res, next) {
+  const { data: result = {} } = req.body;
+  const requiredFields = ["name", "description", "price", "image_url"];
+  for (const field of requiredFields) {
+    if (!result[field]) {
+      return next({
+        status: 400,
+        message: `Dish must include a ${field}`,
+      });
+    }
+  }
+  if (!Number.isInteger(result.price) || result.price < 1) {
+    next({
+      status: 400,
+      message: `Dish must have a price that is an integer greater than 0`,
+    });
+  }
+  next();
+}
+
+function dishIsUpdated(req, res, next) {
+  const { data: updatedDish } = ({} = req.body);
+  const { dishId } = req.params;
+  if (updatedDish.id && updatedDish.id !== dishId) {
+    next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${updatedDish.id}, Route: ${dishId}`,
+    });
+  }
+  next();
+}
+
+function create(req, res) {
+  const { data: result = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    ...result,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function read(req, res) {
+  res.json({ data: res.locals.dish });
+}
+
+function update(req, res) {
+  let foundDish = res.locals.dish;
+  const { data: updatedDish } = req.body;
+  if (!updatedDish.id) {
+    foundDish = {
+      ...updatedDish,
+      id: foundDish.id,
+    };
+  } else {
+    foundDish = { ...updatedDish };
+  }
+  res.json({ data: foundDish });
+}
+
+module.exports = {
+  create: [dishIsValid, create],
+  read: [dishIdExists, read],
+  update: [dishIdExists, dishIsValid, dishIsUpdated, update],
+  list,
+};
---
+++ src/orders/orders.controller.js
@@ -0,0 +1,128 @@
+const path = require("path");
+// Use the existing order data
+const orders = require(path.resolve("src/data/orders-data"));
+// Use this function to assigh ID's when necessary
+const nextId = require("../utils/nextId");
+
+// TODO: Implement the /orders handlers needed to make the tests pass
+
+function orderIdExists(req, res, next) {
+    const { orderId } = req.params;
+    const foundOrder = orders.find((order) => order.id === orderId);
+    if(foundOrder) {
+        res.locals.order = foundOrder;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `No order found with id: ${orderId}`
+    })
+}
+
+function orderIsValid(req, res, next) {
+    const {data: order} = req.body;
+    const requiredFields = ["deliverTo", "mobileNumber", "dishes"];
+    requiredFields.forEach((field) => {
+        if (!order[field]) {
+            return next({
+                status: 400,
+                message: `Order must include a ${field}`
+            })
+        }
+    })
+    if(!Array.isArray(order.dishes) || !order.dishes.length) {
+        return next({
+            status: 400,
+            message: `Order must include at least one dish`,
+        })
+    }
+    order.dishes.forEach((dish, index) => {
+        if(!dish.quantity || dish.quantity <1 || !Number.isInteger(dish.quantity)) {
+            return next({
+                status: 400,
+                message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+            })
+        }
+    })
+    next();
+}
+
+function create(req, res) {
+    const {data: order} = req.body;
+    const newOrder = {
+        ...order,
+        id: nextId()
+    }
+    orders.push(newOrder);
+    res.status(201).json({data: newOrder})
+}
+
+function list(req, res) {
+    res.json({data: orders})
+}
+
+function read(req, res) {
+    res.json({data: res.locals.order})
+}
+
+function destroy(req, res, next) {
+    const {orderId} = req.params;
+    const index = orders.findIndex((order) => order.id === orderId);
+    const currentOrder = orders[index];
+    if (currentOrder.status === "pending") {
+        orders.splice(index, 1);
+        res.sendStatus(204);
+    } else {
+        return next({
+            status: 400,
+            message: "Cannot delete an order that is not pending"
+        })
+    }
+}
+
+function orderUpdateValidated(req, res, next) {    
+    let order = res.locals.order;
+    const { data: attemptedUpdate } = req.body;
+    const {orderId} = req.params;
+    if(attemptedUpdate.id && orderId !== attemptedUpdate.id) {
+        next({
+            status: 400,
+            message: `Order id does not match route id. Order: ${attemptedUpdate.id}, Route: ${orderId}`,
+        })
+    }
+    if (!attemptedUpdate.status || attemptedUpdate.status !== "pending") {
+        next({
+            status: 400,
+            message: "Order must have a status of pending, preparing, out-for-delivery, delivered",
+        })
+    }
+    if(order.status === "delivered") {
+        next({
+            status: 400,
+            message: "A delivered order cannot be changed",
+        })
+    }
+    next();
+}
+
+function update(req, res) {
+    let order = res.locals.order;
+    const { data: attemptedUpdate } = req.body;
+    if (!attemptedUpdate.id) {
+        order = {
+            ...attemptedUpdate,
+            id: order.id,
+        }
+    } else {
+        order = {...attemptedUpdate}
+    }
+    res.json({ data: order })
+}
+
+module.exports = {
+    list,
+    read: [orderIdExists, read],
+    create: [orderIsValid, create],
+    delete: [orderIdExists, destroy],
+    update: [orderIdExists, orderIsValid, orderUpdateValidated, update]
+}
